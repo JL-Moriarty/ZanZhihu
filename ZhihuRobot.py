96aba8e2a68c725e3b024d9aab787410fc873358
RaulWei <674877404@qq.com>
2015-06-01 23:08:46 +0800

    Add Zan Def

--- ZhihuRobot.py
+++ ZhihuRobot.py
@@ -15,12 +15,15 @@ class ZhihuRobot:
     def __init__(self, username, password):
         self.username = username
         self.password = password
+        self.s = requests.session()
+        self._xsrf = BeautifulSoup(self.s.get('http://www.zhihu.com/').content).find(type='hidden')['value']
 
+    # 登录
     def login(self):
-        s = requests.session()
-        _xsrf = BeautifulSoup(s.get('http://www.zhihu.com/').content).find(type='hidden')['value']
+        #s = requests.session()
+        #_xsrf = BeautifulSoup(self.s.get('http://www.zhihu.com/').content).find(type='hidden')['value']
         data = {
-            "_xsrf": _xsrf,
+            "_xsrf": self._xsrf,
             "email": self.username,
             "password": self.password,
             "remember": "y"
@@ -36,28 +39,39 @@ class ZhihuRobot:
                           "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.99 Safari/537.36",
             "X-Requested-With": "XMLHttpRequest"
         }
-        cookie = dict(
-            _alicdn_tc2="AQAAAA6A9WSFIwEAk97KAb/853fo9A2r",
-            #_alicdn_tc2="AQAAAC6iPgOPLAAAk97KAaLTyxGHE6Qt",
-            #_alicdn_tc2="AQAAAGNi8yTmaQMAk97KAZYIKdMiHNwh",
-            #_alicdn_tc2="AQAAAKjzPixX5w4Ak97KAXOW9SDbFWEj",
-            z_c0="\"QUJCQ0VVSFpLd2dYQUFBQVlRSlZUZnJlazFVWmppeFBYeDVKOUtDa3otamtpZlFKb0pTR0VnPT0="
-                 "|1433162234|3bd3b970f08854b13e607527febe0c5004ad7a31\""
-        )
         try:
-            r_login = s.post("http://www.zhihu.com/login", data=data, cookie=cookie)
-            print(r_login.text)
+            r_login = self.s.post("http://www.zhihu.com/login", data=data)
+            #print(r_login.text)
         except Exception as e:
             print("login error")
             sys.exit(-1)
+        r_login = self.s.get('http://www.zhihu.com')
+        #print(r_login.text)
         return
 
     # 点赞
-    def zan(self):
+    def zan(self, answer_url):
+        r_user_answer = self.s.get(answer_url)
+        #print(r_user_answer.content)
+        answer_id = BeautifulSoup(r_user_answer.content).findAll('div', attrs={'class': 'zm-item-answer '})
+        for item in answer_id:
+            print(item['data-aid'])
+            data = {
+                'method': 'vote_up',
+                'params': {'answer_id': item['data-aid']},
+                '_xsrf': '04c3db26d83e1de7b382f27772522f99'
+            }
+            header = {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 '
+                              '(KHTML, like Gecko) Chrome/43.0.2357.81 Safari/537.36'
+            }
+            print(requests.post('http://www.zhihu.com/node/AnswerVoteBarV2', data=data))
+        #print(answer_id)
         return
 
 if __name__ == '__main__':
     robot = ZhihuRobot('weimw0417@163.com', 'admin123456')
     robot.login()
+    robot.zan('http://www.zhihu.com/people/mingwei-wei/answers')
 
 
