5a629db798a00e461800cbc85b4c700348483265
RaulWei <674877404@qq.com>
2015-06-02 20:27:17 +0800

    Update Zan Def 200 OK

--- ZhihuRobot.py
+++ ZhihuRobot.py
@@ -3,11 +3,15 @@ __author__ = 'Wang'
 
 '''
 知乎机器人
+给定用户的答案页面如‘http://www.zhihu.com/people/username/answers’
+自动将该用户所有答案点赞
 '''
 
 import sys
 import requests
+import json
 from BeautifulSoup import BeautifulSoup
+from time import sleep
 
 class ZhihuRobot:
 
@@ -17,11 +21,10 @@ class ZhihuRobot:
         self.password = password
         self.s = requests.session()
         self._xsrf = BeautifulSoup(self.s.get('http://www.zhihu.com/').content).find(type='hidden')['value']
+        print(self._xsrf)
 
     # 登录
     def login(self):
-        #s = requests.session()
-        #_xsrf = BeautifulSoup(self.s.get('http://www.zhihu.com/').content).find(type='hidden')['value']
         data = {
             "_xsrf": self._xsrf,
             "email": self.username,
@@ -40,7 +43,6 @@ class ZhihuRobot:
         }
         try:
             r_login = self.s.post("http://www.zhihu.com/login", data=data, headers=header)
-            #print(r_login.text)
         except Exception as e:
             print("login error")
             sys.exit(-1)
@@ -52,24 +54,38 @@ class ZhihuRobot:
     def zan(self, answer_url):
         r_user_answer = self.s.get(answer_url)
         answer_id = BeautifulSoup(r_user_answer.content).findAll('div', attrs={'class': 'zm-item-answer '})
-        print (self._xsrf)
+        _xsrf = BeautifulSoup(r_user_answer.content).find('input', attrs={'name': '_xsrf'})['value']
+        #print(self._xsrf)
+        #print(_xsrf)
         for item in answer_id:
             print(item['data-aid'])
+            # 以下对params进行json处理是胜败的关键 之前一直返回400 改完这个就200了
+            params = json.dumps({
+                'answer_id': item['data-aid']
+            })
             data = {
                 'method': 'vote_up',
-                'params': {'answer_id': item['data-aid']},
-                '_xsrf': self._xsrf
+                'params': params,
+                '_xsrf': _xsrf
             }
             header = {
+                'Connection': 'keep-alive',
+                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                'Host': 'www.zhihu.com',
+                'Origin': 'http://www.zhihu.com',
+                'Referer': 'http://www.zhihu.com/people/mingwei-wei/answers',
                 'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) '
-                              'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.81 Safari/537.36'
+                              'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.81 Safari/537.36',
+                'X-Requested-With': 'XMLHttpRequest'
             }
             print(self.s.post('http://www.zhihu.com/node/AnswerVoteBarV2', data=data, headers=header, timeout=10))
+            sleep(2)
+
         return
 
 if __name__ == '__main__':
     robot = ZhihuRobot('weimw0417@163.com', 'admin123456')
     robot.login()
-    robot.zan('http://www.zhihu.com/people/mingwei-wei/answers')
+    robot.zan('http://www.zhihu.com/people/wuxu92/answers')
 
 
